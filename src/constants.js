96a4bbc6f77dbc8857adbcfad0167a3154333b50
imsushmoy <hiraku.kazuma5@gmail.com>
2024-06-17 23:15:17 +0600

    organize and set texts

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my delightful PC where I learn and work on various projects related to web development.
+    Here is the link to my <a href="https://github.com/sushcod3" target="_blank">Github</a>!`,
+  "cs-degree": `This is my associate degree certification in computer science. I hung it on the wall because I'm proud of it!`,
+  "sofa-table": `That's my sofa. I like to relax here and read non-fiction books!`,
+  tv: `That's my TV. I love watching series and movies in my free time!`,
+  bed: `This is where I sleep. Sleep is one of the most important part of our life!`,
+  resume: `This is my <a href="https://github.com/sushcod3/resume/blob/main/Mahmud_Hossain_Sushmoy_Front-End_Engineer_Resume.pdf" target="_blank">resume.</a>
+  Contact me at m.hossain.sushmoy@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of books on my shelves. Some of them were read and some of them weren't yet.`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
+};
