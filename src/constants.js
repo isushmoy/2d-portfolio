852386cd75a1c381de5494674643ae9de8609a54
imsushmoy <hiraku.kazuma5@gmail.com>
2024-06-17 23:18:21 +0600

    grammar fix

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ export const dialogueData = {
   "cs-degree": `This is my associate degree certification in computer science. I hung it on the wall because I'm proud of it!`,
   "sofa-table": `That's my sofa. I like to relax here and read non-fiction books!`,
   tv: `That's my TV. I love watching series and movies in my free time!`,
-  bed: `This is where I sleep. Sleep is one of the most important part of our life!`,
+  bed: `This is where I sleep. Sleep is one of the most fundamental aspect of our life!`,
   resume: `This is my <a href="https://github.com/sushcod3/resume/blob/main/Mahmud_Hossain_Sushmoy_Front-End_Engineer_Resume.pdf" target="_blank">resume.</a>
   Contact me at m.hossain.sushmoy@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
